3a96f27195352816545f0f59aea2a623f1981095
parkWanSik <parkws@salin.co.kr>
2023-03-10 14:52:42 +0900

    Root에 의존성 생성 추가

--- Assets/Scripts/Main/SceneRoot.cs
+++ Assets/Scripts/Main/SceneRoot.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class SceneRoot : MonoBehaviour
 {
     [SerializeField] private WorldManager worldManager = null;
+    [SerializeField] private PlayerManager playerManager = null;
+
+    private FollowCamera followCamera = null;
 
     private void Awake()
     {
+        Debug.Assert(playerManager, "PlayerManager is Null !!");
         Debug.Assert(worldManager, "WorldManager is Null !!");
+
+        if(!followCamera)
+            followCamera = Camera.main.GetComponent<FollowCamera>();
+
+        Debug.Assert(followCamera, "FollowCamera is Null !!");
+
+        
     }
 }

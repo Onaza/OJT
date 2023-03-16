36ab62e53775dda04f1e17f4d668bccbc55a694e
parkws <parkws@salin.co.kr-at-513087882511>
2023-03-16 11:32:48 +0900

    스크롤뷰 클릭 시 서버 이름 저장 하도록 수정

--- Assets/Scripts/Scenes/Lobby/LobbySceneRoot.cs
+++ Assets/Scripts/Scenes/Lobby/LobbySceneRoot.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class LobbySceneRoot : MonoBehaviour
 {
+    [SerializeField] private PlayerManager playerPrefab = null;
     [SerializeField] private LobbySceneUIRoot uiRoot = null;
 
     private void Awake()
     {
         Debug.Assert(uiRoot, "UIRoot is Null !!");
+
+        Instantiate<PlayerManager>(playerPrefab);
     }
 }

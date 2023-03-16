f50b4af66e9f4d851b7950438b0066cd716d3951
parkws <parkws@salin.co.kr-at-513087882511>
2023-03-16 11:10:23 +0900

    서버리스트 리스폰스 추가

---
+++ Assets/Scripts/Common/Network/Response/ServerListResponse.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ServerListResponse : MonoBehaviour
+{
+    public List<Data> rows;
+
+    [System.Serializable]
+    public struct Data
+    {
+        public string serverId;
+        public string serverName;
+    }
+}
